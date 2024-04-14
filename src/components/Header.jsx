fc27244c6f88a2e31bda72ea8c6451c38b55f9d5
EricGueCh <84349917+EricGueCh@users.noreply.github.com>
2024-04-14 12:53:27 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const Header=()=>{
     return(
         <div className="header">
-            <img src="/src/img/logo_principal.png" alt=""/>
+            <img src="https://images.vexels.com/media/users/3/136295/isolated/preview/9293787a09b95e84c856e6474cff8ce0-marca-de-licorer-a.png" alt=""/>
             <ul>
                 <li><Link to="/">Inicio</Link></li>
                 <li><Link to="/Productos">Nuestros Productos</Link></li>
